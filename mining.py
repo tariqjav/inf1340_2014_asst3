39d49b1ca51f36fe88fc97081a2bc80190c29d2d
INF1340-NKT <nazaninkt.khosravanitehrani@gmail.com>
2014-11-29 12:51:29 -0500

    Volume and Closing data read and avg computed - Comments added - TODO Pick Top and Worst avgs

--- mining.py
+++ mining.py
@@ -21,14 +21,46 @@ monthly_averages = []
 total_sales = {}
 total_volume = {}
 
+def compute_avg(data, list_close):
+    '''
+
+    :param data: month of the year
+    :param list: list of tuple of (closing and volume)
+    :return: average
+    '''
+    sum = 0
+    vol_total = 0
+    for key in list_close:
+        sum += key[0] * key[1]
+        vol_total += key[1]
+
+    return sum / vol_total
 
 def read_stock_data(stock_name, stock_file_name):
+    '''
+    read_stock_data
+    :param stock_name: The name of the stock
+    :param stock_file_name: The name of the file holding the stock values.
+    :return:
+
+    '''
+
+    # Check Types of arguments passed in
+    if not type(stock_name) is str:
+        raise TypeError("Type Error: Please Pass in stock name in string")
+
+
+    if not type(stock_file_name) is str:
+        raise TypeError("Type Error: Please Pass in stock file name in string")
+
     #Telling python to read from the stock file
     with open(stock_file_name) as file_handle:
         file_contents = file_handle.read()
         # Storing the entire contents of the json file into a list variable
         stock_data = json.loads(file_contents)
         #Telling python to iterate through each dictionary in the data
+        dict_elem = {}
+        prev_date = 0
         for element in stock_data:
             #Telling python to get the date and strip it as month/year
             date_entry = element.get("Date")
@@ -36,48 +68,56 @@ def read_stock_data(stock_name, stock_file_name):
             stock_date = datetime.datetime.strftime(parse_date,"%m-%Y")
 
             #Telling python to get the volume and the closing price
-            #volume =[]
-            #closing_price=[]
-            #volume = element.get("Volume")
-            #closing_price = element.get("Close")
 
-            #Telling python to append the month/year into our total_sales dictionary as the key
-            if stock_date in total_volume:
-                stock_date = element.get("Date")
+            volume = element.get("Volume")
+            closing_price = element.get("Close")
+
+            # compare and see if the date of the element just read is the same as the previous
+            # element date or not, if not, we can compute the average now as we have read the data
+            # for all the month.
+
+            # TO DO: Once comparison is done, we can compare it with TOP 6 months that we have so
+            # far, if its better we update the TOP 6 months to include this one and drop the 7th one
+
+            # TO DO: We also have to do the comparison to TOP worst 6 months and include this month
+            # in those month is the avg is very low
+
+            if stock_date == prev_date:
+                dict_elem[stock_date].append((closing_price, volume))
             else:
-                total_volume[stock_date] = []
+                if prev_date in dict_elem:
+                    print(compute_avg(prev_date, dict_elem[prev_date]))
+                dict_elem[stock_date] = [(closing_price, volume)]
+
+            # Keep what is previous date.
+            prev_date = stock_date
 
 
 
-
-            #Telling python to get the stock volume
-            #stock_volume = []
-            #stock_volume = element.get("Volume")
-            #stock_closing_price = []
-            #stock_closing_price = element.get("Close")
-
-
-
-            #Telling python to append the month/year into our total_volume dictionary as the key
-
-                #else:
-                    #total_volume[stock_date] = " "
-
-
-            #stock_closing_price = element.get("Close")
-
-        return print(total_volume)
+        print(dict_elem)
 
 
 def six_best_months():
+    '''
+    :return:
+    '''
     return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
 
 def six_worst_months():
+    '''
+
+    :return:
+    '''
     return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
 
 def read_json_from_file(file_name):
+    '''
+
+    :param file_name:
+    :return:
+    '''
     with open(file_name) as file_handle:
         file_contents = file_handle.read()
         # Storing the entire contents of the json file into a list variable
